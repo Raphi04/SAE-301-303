1f63dd8ca814be42bbbaa9743a0ba4a42bb2bbb4
Raphi04 <raphaelcadete04@gmail.com>
2024-01-01 13:22:57 +0100

    test database

--- Sprint5/site/php/test.php
+++ Sprint5/site/php/test.php
@@ -1,11 +1,34 @@
 <?php
-require("acf2l_traitement.php");
-require("acf2l_models.php");
-require("acf2l_database.php");
+include "database.php";
 
-$sql = "SELECT * FROM personne";
-$requete1 = new Traitement();
-$requete1->requete($sql, null);
-echo $requete1;
+$allVariable = ["nom", "prenom"];
+$errors = [];
 
+function checkError() {
+    if(array_key_exists("send",$_POST)) {
+        global $allVariable;
+        global $errors;
+        foreach($allVariable as $variable) {
+            if ($_POST[$variable] == "") {
+                array_push($errors, $variable);
+            }
+        }
+    }
+}
+?>
+
+<form action="test.php" method="post">
+    <label for="nom">Nom</label>
+    <input id="nom" name="nom" type="text" placeholder="Nom">
+    <label for="nom">Prenom</label>
+    <input id="prenom" name="prenom" type="text" placeholder="Prenom">
+    <button type="submit" name="send">Envoyer</button>
+</form>
+<?php
+    checkError();
+    var_dump($errors);
+    var_dump($_POST);
+    if(count($errors) > 0){
+        echo "erreur";
+    }
 ?>
