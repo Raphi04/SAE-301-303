5189af755b734ecf8c1dbcfc727c956b5274c1eb
Raphi04 <raphaelcadete04@gmail.com>
2024-01-01 18:41:16 +0100

    FUSION

--- Sprint5/site/php/functions.php
+++ Sprint5/site/php/functions.php
@@ -1,29 +1,118 @@
 <?php
+//Declaration des variables 
+$currentDate;
+$nextYear;
+$errors = [];
+$errorMessage;
+
+///////////////////////////////////
+////// GESTION DE FORMULAIRE //////
+//////////////////////////////////
+
+function calculateDate() {
+    global $currentDate;
+    global $nextYear;
+    $date = getdate();
+    if ($date["mon"] < 10) {
+        $date["mon"] = "0".$date["mon"];
+    }
+    if ($date["mday"] < 10) {
+        $date["mday"] = "0".$date["mday"];
+    }
+    $currentDate = $date["year"]."-".$date["mon"]."-".$date["mday"];
+    $nextYear = intval($date["year"]+1)."-".$date["mon"]."-".$date["mday"];
+    var_dump($currentDate);
+    var_dump($nextYear);
+}
+
 function detectError($attendus) {
-    $errors = [];
+    global $currentDate;
+    global $errorMessage;
+    global $errors;
     if (array_key_exists("send", $_POST)) {
         foreach($attendus as $attendu) {
             if($_POST[$attendu] == "") {
                 array_push($errors, $attendu);
             }
         }
-        if(count($errors) > 0){
-            echo "erreur dans $errors[0]";
-        } else {
-            echo "pas d'erreur";
+        if(count($errors) > 1){
+            $errorMessage = "Il y a une plusieurs erreurs dans le formulaire.";
+        } elseif(count($errors) > 0){
+            $errorMessage = "Il y a une erreur dans le champs $errors[0].";
+        }else {
+            if (array_key_exists("email",$_POST) && !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+                array_push($errors, $_POST["email"]);
+                $errorMessage = "Veuillez entrer une adresse mail correcte.";
+            }else {
+                $errorMessage = "Il n'y a aucune erreur.";
+            }
+        }
+    }
+    var_dump($errors);
+}
+
+function errorClass($input){
+    global $errors;
+    if (in_array($input, $errors)) {
+        echo "class=\"redBorder\"";
+    } elseif ($input == "email")
+        if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        echo "class=\"redBorder\"";
+    }
+}
+
+function errorMessage(){
+    global $errorMessage;
+    if (!empty($errorMessage)) {
+        echo "<p class=\"red\">$errorMessage</p>";
+    }
+}
+
+function ancientValue($input) {
+    if (!empty($_POST[$input])) {
+        echo "value=\"$_POST[$input]\"";
+    }
+}
+
+function generateOption($options) {
+    if (array_key_exists("where", $_POST)) {
+        echo "<option value=".$_POST["where"].">Baptême de l'air -".$_POST["where"]."</option>";
+        foreach($options as $option) {
+            if ($option !== $_POST["where"]) {
+                echo "<option value=\"$option\">Baptême de l'air - $option</option>";
+            }
+        }
+    } elseif (array_key_exists("type", $_POST)) {
+        echo "<option value=".$_POST["type"].">Baptême de l'air -".$_POST["type"]."</option>";
+        foreach($options as $option) {
+            if ($option !== $_POST["type"]) {
+                echo "<option value=\"$option\">Baptême de l'air - $option</option>";
+            }
+        }
+     } else {
+        foreach($options as $option) {
+            echo "<option value=\"$option\">Baptême de l'air - $option</option>";
         }
     }
 }
 
-function checkError() {
-    if(array_key_exists("send",$_POST)) {
-        global $allVariable;
-        global $errors;
-        foreach($allVariable as $variable) {
-            if ($_POST[$variable] == "") {
-                array_push($errors, $variable);
-            }
-        }
-    }
+//////////////////
+////// SQL //////
+////////////////
+
+function requeteSQL($sql) {
+    global $db;
+    $prepare = $db->prepare($sql);
+    
+
+
+    $getItemNameSql = "SELECT nom,type FROM article WHERE reference=:reference";
+    $getItemName = $db->prepare($getItemNameSql);
+    $sqlParameter = [
+        "reference" => $reference
+    ];
+    $getItemName->execute($sqlParameter) or die($db->errorInfo());
+    $results=$getItemName->fetchAll(PDO::FETCH_ASSOC);
+    $itemName=$results[0]["nom"];
 }
 ?>
--- Sprint5/site/php/test.php
+++ Sprint5/site/php/test.php
@@ -1,21 +1,47 @@
+<style>
+    .redBorder {
+        border: 2px solid red;
+    }
+    .red {
+        color: red;
+    }
+</style>
 <?php
 include "database.php";
 include "functions.php";
-$allVariable = ["nom", "prenom"];
-$errors = [];
+$allVariable = ["nom", "prenom", "email", "telephone", "type", "reserv"];
+$options = ["Pendulaire", "Autogire", "Axes"];
+calculateDate();
+detectError($allVariable);
+var_dump($_POST);
+if (!array_key_exists("send", $_POST) || count($errors) > 0) { 
+    ?>
+    <form action="test.php" method="post">
+        <label for="nom">Nom</label>
+        <input <?php errorClass("nom") ?> id="nom" name="nom" type="text" placeholder="Nom" <?php ancientValue("nom") ?> >
 
+        <label for="nom">Prenom</label>
+        <input <?php errorClass("prenom") ?> id="prenom" name="prenom" type="text" placeholder="Prenom" <?php ancientValue("prenom") ?> >
+        
+        <label for="email">Email</label>
+        <input <?php errorClass("email") ?> id="email" name="email" type="text" placeholder="Email" <?php ancientValue("email") ?> >
 
-?>
+        <label for="telephone">Téléphone</label>
+        <input <?php errorClass("telephone") ?> id="telephone" name="telephone" type="number" placeholder="Numéro de téléphone" <?php ancientValue("telephone") ?> >
 
-<form action="test.php" method="post">
-    <label for="nom">Nom</label>
-    <input id="nom" name="nom" type="text" placeholder="Nom">
-    <label for="nom">Prenom</label>
-    <input id="prenom" name="prenom" type="text" placeholder="Prenom">
-    <button type="submit" name="send">Envoyer</button>
-</form>
-<?php
-    detectError($allVariable);
-    var_dump($errors);
-    var_dump($_POST);
+        <label for="type">Type</label>
+        <select name="type" id="type">
+            <?php generateOption($options) ?>
+        </select>
+        
+        <label for="reserv">Date de Réservation</label>
+        <input <?php errorClass("reserv") ?> type="date" name="reserv" id="reserv" min="<?php echo "$currentDate" ?>" max="<?php echo "$nextYear" ?>" <?php ancientValue("reserv") ?> >
+
+        <button type="submit" name="send">Envoyer</button>
+    </form>
+    <?php
+    errorMessage();
+} else {
+    requeteSQL();
+}
 ?>
