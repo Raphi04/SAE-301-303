822ce6c6e71953c9748e72f70f2c88d2301b66bf
Raphi04 <raphaelcadete04@gmail.com>
2024-01-05 14:52:18 +0100

    database à jour

--- Sprint6/site/acf2l.sql
+++ Sprint6/site/acf2l.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 04 jan. 2024 à 10:48
+-- Généré le : ven. 05 jan. 2024 à 13:51
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -41,15 +41,16 @@ CREATE TABLE IF NOT EXISTS `adherent` (
 --
 
 INSERT INTO `adherent` (`email`, `prenom`, `nom`, `telephone`) VALUES
-('a@a.com', 'a', 'a', '1'),
-('a@gmail.com', 'a', 'a', '11'),
-('b@b.com', 'a', 'a', '1'),
-('b@gmail.com', 'a', 'a', '11'),
-('cghjk@gmail.com', 'raphael', 'cadete', '47521'),
-('fevcdhjb@gmail.com', 'vfdezjsk', 'cgvhbjnkl', '1456321'),
-('raphaelcadete04@gmail.com', 'Raphael', 'Cadete', '0178585636'),
-('uwu@gmail.com', 'a', 'a', '03'),
-('z@zmail.com', 'z', 'z', '8');
+('emanuelmacron@gmail.com', 'Emanuel', 'Macron', '0698741256'),
+('erickzemour@gmail.com', 'Erick', 'Zemour', '0696987421'),
+('hbajoue@gmail.com', 'Hugo', 'Bajoue', '0685749636'),
+('jeanlucmelanchon@gmail.com', 'Jean-luc', 'Mélanchon', '068741236'),
+('jeanpatrick@gmail.com', 'Jean', 'Patrick', '0685123596'),
+('jeanpaul@gmail.com', 'Jean', 'Paul', '0625743912'),
+('justinedelafontaine@gmail.com', 'Justine', 'De Lafontaine', '0685742132'),
+('martinedupont@gmail.com', 'Martine', 'Dupont', '0685749632'),
+('paulmartin@gmail.com', 'Paul', 'Martin', '0687416932'),
+('raphaelcadete04@gmail.com', 'Raphael', 'Cadete', '0178585636');
 
 -- --------------------------------------------------------
 
@@ -84,6 +85,18 @@ CREATE TABLE IF NOT EXISTS `pilote` (
   PRIMARY KEY (`identifiant`,`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Déchargement des données de la table `pilote`
+--
+
+INSERT INTO `pilote` (`identifiant`, `type`) VALUES
+(2, 'Pendulaire'),
+(3, 'AutoGire'),
+(9, 'Pendulaire'),
+(10, 'AutoGire'),
+(11, 'Axes'),
+(12, 'Axes');
+
 -- --------------------------------------------------------
 
 --
@@ -93,26 +106,33 @@ CREATE TABLE IF NOT EXISTS `pilote` (
 DROP TABLE IF EXISTS `reservation`;
 CREATE TABLE IF NOT EXISTS `reservation` (
   `numReserv` int NOT NULL AUTO_INCREMENT,
-  `mail` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `matricule` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `identifiant` int DEFAULT NULL,
   `dateReserv` date DEFAULT NULL,
   `type` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `statut` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`numReserv`),
-  KEY `mail` (`mail`),
-  KEY `matricule` (`matricule`),
   KEY `identifiant` (`identifiant`),
-  KEY `type` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `type` (`type`),
+  KEY `reservation_ibfk_1` (`email`),
+  KEY `reservation_ibfk_2` (`matricule`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `reservation`
 --
 
-INSERT INTO `reservation` (`numReserv`, `mail`, `matricule`, `identifiant`, `dateReserv`, `type`, `statut`) VALUES
-(1, 'raphaelcadete04@gmail.com', NULL, NULL, '2024-01-02', 'Pendulaire', 'Attente'),
-(2, 'raphaelcadete04@gmail.com', NULL, NULL, '2024-01-10', 'Autogire', 'Attente');
+INSERT INTO `reservation` (`numReserv`, `email`, `matricule`, `identifiant`, `dateReserv`, `type`, `statut`) VALUES
+(3, 'emanuelmacron@gmail.com', 'AXB-185-PML', 2, '2024-01-17', 'Pendulaire', 'Traité'),
+(6, 'raphaelcadete04@gmail.com', 'AXB-185-PML', 2, '2024-01-04', 'Pendulaire', 'Traité'),
+(7, 'raphaelcadete04@gmail.com', 'AXB-185-PML', 2, '2024-01-06', 'Pendulaire', 'Traité'),
+(8, 'raphaelcadete04@gmail.com', 'NBB-275-JUL', 9, '2024-01-06', 'Pendulaire', 'Traité'),
+(9, 'raphaelcadete04@gmail.com', 'NBB-275-JUL', 9, '2024-01-19', 'Pendulaire', 'Traité'),
+(10, 'raphaelcadete04@gmail.com', 'AXB-185-PML', 2, '2024-02-02', 'Pendulaire', 'Traité'),
+(12, 'raphaelcadete04@gmail.com', 'AXB-185-PML', 2, '2024-01-05', 'Pendulaire', 'Traité'),
+(13, 'raphaelcadete04@gmail.com', NULL, NULL, '2024-01-08', 'Pendulaire', 'En attente'),
+(15, 'raphaelcadete04@gmail.com', NULL, NULL, '2024-01-06', 'Pendulaire', 'En attente');
 
 -- --------------------------------------------------------
 
@@ -127,7 +147,21 @@ CREATE TABLE IF NOT EXISTS `staff` (
   `nom` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `role` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`identifiant`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `staff`
+--
+
+INSERT INTO `staff` (`identifiant`, `prenom`, `nom`, `role`) VALUES
+(2, 'Jean', 'Patrick', 'Pilote'),
+(3, 'Jean', 'Michel', 'Pilote'),
+(6, 'Jean', 'Micheline', 'Secretaire'),
+(8, 'Jean', 'Eude', 'Météorologiste'),
+(9, 'Jean', 'Polnareff', 'Pilote'),
+(10, 'Jean', 'Giovana', 'Pilote'),
+(11, 'Jean', 'Joestar', 'Pilote'),
+(12, 'Jean', 'Kakyoin', 'Pilote');
 
 -- --------------------------------------------------------
 
@@ -141,6 +175,18 @@ CREATE TABLE IF NOT EXISTS `vehicule` (
   `type` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`matricule`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `vehicule`
+--
+
+INSERT INTO `vehicule` (`matricule`, `type`) VALUES
+('AXB-185-PML', 'Pendulaire'),
+('CVG-256-SDF', 'Axes'),
+('IOP-789-MLK', 'Axes'),
+('NBB-275-JUL', 'Pendulaire'),
+('QSD-147-LPD', 'AutoGire'),
+('RTO-963-GFD', 'AutoGire');
 
 --
 -- Contraintes pour les tables déchargées
@@ -157,9 +203,9 @@ ALTER TABLE `pilote`
 -- Contraintes pour la table `reservation`
 --
 ALTER TABLE `reservation`
-  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`mail`) REFERENCES `adherent` (`email`),
-  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`matricule`) REFERENCES `vehicule` (`matricule`),
-  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`identifiant`) REFERENCES `pilote` (`identifiant`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`email`) REFERENCES `adherent` (`email`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`matricule`) REFERENCES `vehicule` (`matricule`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`identifiant`) REFERENCES `pilote` (`identifiant`) ON DELETE RESTRICT ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
